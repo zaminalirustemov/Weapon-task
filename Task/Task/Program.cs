b35128950a44b7e2e7c43071481f9bdfd37c9011
zaminalirustemov <2g17rck@code.edu.az>
2022-11-24 11:04:42 +0400

    Homework

---
+++ Task/Task/Program.cs
@@ -0,0 +1,114 @@
+﻿using System.Diagnostics.SymbolStore;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletCapacity;
+            int bulletCount;
+            double dechargeTime;
+            bool isAuto;
+            bool check;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(">>>>>>>WELCOME TO ARMORY<<<<<<<");
+            do
+            {
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.Write("---Enter the bulletcapacity:");
+                check = int.TryParse(Console.ReadLine(), out bulletCapacity);
+                if (bulletCapacity<=0)
+                {
+                    check = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: Bullet capacity is a number and must be greater than 0");
+                }
+
+            } while (!check);
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("---Enter the bulletcount:");
+                check = int.TryParse(Console.ReadLine(), out bulletCount);
+                if (bulletCount > bulletCapacity || bulletCount<0)
+                {
+                    check = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: Bullet count is a number and cannot be less than 0");
+                }
+
+
+            }while (!check) ;
+            
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("---Enter the dechargetime:");
+                check = double.TryParse(Console.ReadLine(), out dechargeTime);
+                if (dechargeTime <= 0)
+                {
+                    check = false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Xeta: Dechargetime is a number and must be greater than 0.");
+                }
+
+            } while (!check);
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("---Enter the mode(true=Auto mode | false=Single mode):");
+                check = bool.TryParse(Console.ReadLine(), out isAuto);
+
+            } while (!check);
+
+            Weapon weapon = new Weapon(bulletCapacity, bulletCount, dechargeTime, isAuto);
+
+            string choise;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("====================================");
+                Console.WriteLine("        Menu:");
+                Console.WriteLine("0 - For get information\r\n1 - For Shoot method\r\n2 - For Fire method\r\n3 - For GetRemainBulletCount method\r\n4 - For Reload method\r\n5 - For ChangeFireMode method\r\n6 - Console clear\r\n7 - Shortcut to quit the program.\"");
+                Console.WriteLine("====================================");
+                Console.Write("Please select:"); choise = Console.ReadLine();
+                Console.WriteLine("------------------------------------");
+                switch (choise)
+                {
+                    case "0":
+                        weapon.ShowAllInfo();
+                        break;
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"The number of bullets needed to fully fill the capacity: {weapon.GetRemainBulletCount()}");
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "6":
+                        Console.Clear();
+                        break;
+                    default:
+                        break;
+                }
+            } while (choise != "7");
+
+        }
+    }
+}
---
+++ Task/Task/Weapon.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Task
+{
+    internal class Weapon
+    {
+        public Weapon(int bulletCapacity, int bulletCount, double dechargeTime, bool ısAuto)
+        {
+            this.BulletCapacity = bulletCapacity;
+            this.BulletCount = bulletCount;
+            this.DechargeTime = dechargeTime;
+            this.IsAuto = ısAuto;
+        }
+
+        public int BulletCapacity { get; set; }
+        public int BulletCount { get; set; }
+        public double DechargeTime { get; set; }
+        public bool IsAuto { get; set; }
+
+        public void ShowAllInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"---BulletCapacity:  {BulletCapacity} \n---BulletCount:     {BulletCount} \n---DechargeTime:    {DechargeTime} \n---Mode:            {IsAuto} ");
+        }
+        public void Shoot()
+        {
+            if (BulletCount > 0)
+            {
+                BulletCount--;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($">>>DISH<<< \nBullet count: {BulletCount} ");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Not enough bullets, reload");
+            }
+
+        }
+        public void Fire()
+        {
+            if (IsAuto == true)
+            {
+                if (BulletCount > 0)
+                {
+                    double NowDechargeTime = (DechargeTime * BulletCount) / BulletCapacity;
+                    BulletCount = 0;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($">>>DIDIDIDIDIDISHHHHH<<< \nCurrently, the period of discharge of the bullets: {NowDechargeTime} second \nBullet count: {BulletCount} pieces (Not enough bullets, reload)");
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Not enough bullets, reload (Bullet count: {BulletCount} )");
+                }
+            }
+            else
+            {
+                Shoot();
+            }
+
+
+        }
+        public int GetRemainBulletCount()
+        {
+            int remainBullet = BulletCapacity - BulletCount;
+            return remainBullet;
+
+
+        }
+        public void Reload()
+        {
+            if (BulletCount < BulletCapacity)
+            {
+                BulletCount = BulletCount + GetRemainBulletCount();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"weapon is reloaded. Bullet:{BulletCount}");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Full of bullets.");
+            }
+
+        }
+        public void ChangeFireMode()
+        {
+            IsAuto = !IsAuto;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Weapon mode changed.");
+
+        }
+    }
+}
